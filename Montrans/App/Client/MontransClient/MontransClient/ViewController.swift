ffc34cd6f787af7538797a0353374454542b64d2
brazilec22 <stratienko.a@yahoo.com>
2020-07-25 23:26:50 +0300

    auto commit

---
+++ Montrans/App/Client/MontransClient/MontransClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MontransClient
+//
+//  Created by  brazilec22 on 25.07.2020.
+//  Copyright © 2020  brazilec22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
