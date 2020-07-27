a8233fd1441600b68ee9cb392e72c026628a8a12
brazilec22 <stratienko.a@yahoo.com>
2020-07-27 14:32:52 +0300

    auto commit

--- coreML/coreML/ViewController.swift
+++ coreML/coreML/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 import AVFoundation
 import Vision
-
+import coreML
 class ViewController: UIViewController {
-
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var resultView: UIView!
+    private var maskLayer = [CAShapeLayer]()
 
     let session = AVCaptureSession()
     var previewLayer: AVCaptureVideoPreviewLayer!
@@ -22,10 +22,13 @@ class ViewController: UIViewController {
     private var detectionOverlay: CALayer! = nil
     var rootLayer: CALayer! = nil
     var bufferSize: CGSize = .zero
+    var posX = CGFloat()
+    var posY = CGFloat()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        posX = view.frame.maxX
+        posY = view.frame.maxY
         guard let camera = AVCaptureDevice.default(for: .video) else {
             return
         }
@@ -35,6 +38,7 @@ class ViewController: UIViewController {
             rootLayer = resultView.layer
             previewLayer.frame = rootLayer.bounds
             rootLayer.addSublayer(previewLayer)
+            
             let cameraInput = try AVCaptureDeviceInput(device: camera)
             let videoOutput = AVCaptureVideoDataOutput()
             videoOutput.setSampleBufferDelegate(self, queue: captureQueue)
@@ -61,8 +65,23 @@ class ViewController: UIViewController {
             alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
             present(alertController, animated: true, completion: nil)
         }
-        self.setupLayers()
-        self.updateLayerGeometry()
+        let layer = CAShapeLayer()
+        let rect =  CGRect(x: 250 - 50, y: 100, width: 100, height: 100)//CGRect(x: posX/2 - 50, y: posY/2, width: 100, height: 100)
+        layer.path = UIBezierPath(roundedRect: rect, cornerRadius: 0).cgPath
+        layer.fillColor = UIColor.red.cgColor
+        layer.opacity = 0.5
+        
+        resultView.layer.addSublayer(layer)
+        
+        //let shapeLayer = self.createRoundedRectLayerWithBounds(rect)
+        
+//        resultView.layer.addSublayer(shapeLayer)
+
+        
+
+        
+       // self.setupLayers()
+       // self.updateLayerGeometry()
     }
 
     override func viewDidLayoutSubviews() {
@@ -85,21 +104,60 @@ class ViewController: UIViewController {
         results[0...3].forEach {
             let identifer = $0.identifier.lowercased()
             let confidence = $0.confidence
-
+            if identifer == "paper towel" {
+                //let bounds = CGRect(x: view.frame.maxX/2 - 50, y: view.frame.maxY/2, width: 150, height: 150)
+                self.updateMainLayer(/*layer: layer*/)
+                resultString = "\(identifer) : \(confidence)"
+            }
            // if identifer.range(of: "cat") != nil || identifer.range(of: "cat") != nil || //identifer == "cat" {
     //out            print("id results: \(identifer) - \(confidence)")
-                resultString = "\(identifer) : \(confidence)"
            // }
         }
         DispatchQueue.main.async {
             self.resultLabel.text = resultString
         }
     }
+    func updateMainLayer(/*layer : CALayer*/) {
+        let layer = CAShapeLayer()
+        let rect = CGRect(x: 250 - 50, y: 100, width: 100, height: 100)
+        print(rect)
+        layer.path = UIBezierPath(roundedRect: rect, cornerRadius: 0).cgPath
+        layer.fillColor = UIColor.blue.cgColor
+        layer.opacity = 0.5
+        self.resultView.layer.addSublayer(layer)
+    }
     func drawVisionRequestResults(_ results: [Any]) {
         CATransaction.begin()
         CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
         detectionOverlay.sublayers = nil // remove all the old recognized objects
       //  print("out for :: \(results)")
+        
+     //    print("type : \(type(of: results))")
+        guard let observations = results as? [VNClassificationObservation] else { print("is VNCoreMLFeatureValueObservation")
+            return
+        }
+     //   print("type array : \(type(of: observations))")
+            for observation in observations /* where observation is VNRecognizedObjectObservation */{
+                guard let objectObservation = observation as? VNClassificationObservation else {
+       //         print("is Observation None")
+                continue }
+            // Select only the label with the highest confidence.
+                       let topLabelObservation = objectObservation//objectObservation.labels[0]
+                       let objectBounds = VNImageRectForNormalizedRect(CGRect(x: 0.0,
+                                                                                         y: 0.0,
+                                  width: bufferSize.width + 1.0,
+                                  height: bufferSize.height + 1.0), Int(bufferSize.width), Int(bufferSize.height))
+                       
+                       let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+                       
+                       let textLayer = self.createTextSubLayerInBounds(objectBounds,
+                                                                       identifier: topLabelObservation.identifier,
+                                                                       confidence: topLabelObservation.confidence)
+                       shapeLayer.addSublayer(textLayer)
+                       detectionOverlay.addSublayer(shapeLayer)
+        }
+         
+       /*
         for observation in results where observation is VNRecognizedObjectObservation {
      //       print("in for :: \(observation)")
             guard let objectObservation = observation as? VNRecognizedObjectObservation else {
@@ -119,7 +177,7 @@ class ViewController: UIViewController {
                                                             confidence: topLabelObservation.confidence)
             shapeLayer.addSublayer(textLayer)
             detectionOverlay.addSublayer(shapeLayer)
-        }
+        } */
         self.updateLayerGeometry()
         CATransaction.commit()
     }
@@ -175,11 +233,37 @@ class ViewController: UIViewController {
         detectionOverlay.name = "DetectionOverlay"
         detectionOverlay.bounds = CGRect(x: 0.0,
                                          y: 0.0,
-                                         width: bufferSize.width,
-                                         height: bufferSize.height)
+                                         width: bufferSize.width + 2,
+                                         height: bufferSize.height + 2)
         detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
         rootLayer.addSublayer(detectionOverlay)
     }
+    
+    /// MARK
+    private func createLayer(in rect: CGRect) -> CAShapeLayer{
+          
+          let mask = CAShapeLayer()
+          mask.frame = rect
+          mask.cornerRadius = 10
+          mask.opacity = 0.75
+          mask.borderColor = UIColor.yellow.cgColor
+          mask.borderWidth = 2.0
+          maskLayer.append(mask)
+          return mask
+      }
+      
+      func drawFaceboundingBox(face : VNClassificationObservation) {
+          
+        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -self.view.frame.height)
+          
+          let translate = CGAffineTransform.identity.scaledBy(x: self.view.frame.width, y: self.view.frame.height)
+          
+          // The coordinates are normalized to the dimensions of the processed image, with the origin at the image's lower-left corner.
+          let facebounds = CGRect(x: 0, y: 0, width: 2, height: 2)
+          
+          _ = createLayer(in: facebounds)
+          
+      }
 }
 
 // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
